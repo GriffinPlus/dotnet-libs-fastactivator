b520b18ea1e89fcdfc2166c3be68a093ac419f94
Sascha Falk <sascha@falk-online.eu>
2021-06-21 12:25:14 +0200

    Update to new version of build system

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -2,4 +2,13 @@
 
 [assembly: AssemblyCompany("Griffin+")]
 [assembly: AssemblyProduct("Griffin+ Fast Activator")]
-[assembly: AssemblyCopyright("Copyright (c) 2020 Sascha Falk and Contributors")]
+[assembly: AssemblyCopyright("Copyright (c) 2021 Sascha Falk and Contributors")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
